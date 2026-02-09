6e5eafa1a0b1d062e9be9b5d2c9155e9be671e26
balaji.a.oxinfy <balaji.a.oxinfy@gmail.com>
2026-02-09 17:25:30 +0530

    change text

--- WEEK-4/tanstack-router-code-based/vite.config.ts
+++ WEEK-4/tanstack-router-code-based/vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({}), react(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/test/setup.ts",
+  },
 });
