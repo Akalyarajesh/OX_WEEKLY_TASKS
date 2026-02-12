533939d88b15a9c25ec4b9f4c901f87eea6dd63c
akalyarajesh <akalyaravichandran08@gmail.com>
2026-02-12 15:44:55 +0530

    Modified by Akalyarajesh

---
+++ shadcn/shadcn-ui/src/App.tsx
@@ -0,0 +1,27 @@
+import { Button } from "./components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Accordion type="single" collapsible defaultValue="item-1">
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
